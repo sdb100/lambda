d23c00938db7315c657ef4b663f28ab7addfa730
Steve Bell <steve.bell@worldpay.com>
2016-06-27 14:34:38 +0100

    Add a test, sort of

---
+++ src/test/java/lambdas/DispatcherTest.java
@@ -0,0 +1,13 @@
+package lambdas;
+
+import org.junit.Test;
+
+public class DispatcherTest {
+    @Test
+    public void testDispatcher(){
+        
+        // Not really a test
+        new Dispatcher().makeCalls();
+    }
+
+}
